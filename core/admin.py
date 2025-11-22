6b6b42797eed30df16f87db8f3d2b901117c5340
Swetha_k <j657z733@wichita.edu>
2025-11-22 11:09:30 +0530

    Register models in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Department, Instructor, Student, Course, Enrollment
 
-# Register your models here.
+admin.site.register(Department)
+admin.site.register(Instructor)
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Enrollment)
